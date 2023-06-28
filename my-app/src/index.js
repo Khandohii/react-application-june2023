51f24e84af86fe874d81f5af8130a673fe1ef944
Dmytro <dmitry.khandogy@gmail.com>
2023-06-28 13:24:09 +0200

    Lesson 122. Работаем с JSX-препроцессором, ограничения в нем

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const elem = <h2>Hello World</h2>;
+
+const text = 'Hello World';
+const elem = (
+  <div>
+    <h2 className='text'>Text: {text}</h2>
+    <input type='text' />
+    <label htmlFor=""></label>
+    <button tabIndex={0}>Click</button>
+  </div>
+);
+
+// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World');
+
+// const element = {
+//   type: 'h2',
+//   props: {
+//     className: 'greetings',
+//     children: "Hello World"
+//   }
+// };
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem,
 );
 
