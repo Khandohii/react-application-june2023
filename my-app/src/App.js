5abd98565167a469e6913fad39e7a160da94609d
Dmytro <dmitry.khandogy@gmail.com>
2023-07-24 12:21:16 +0200

    Lesson 130 - component states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, {StrictMode} from 'react';
+import { Component } from "react";
 import './App.css';
 
 const Header = () => {
@@ -40,13 +41,35 @@ function Btn() {
   return <button>{logged ? 'Enter' : text}</button>
 }
 
-function WhoAmI({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name.firstName}, surname is - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++',
+    }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <div>
+        <br />
+        <br />
+        
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname is - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+
+      </div>
+    )
+  }
 }
 
 function App() {
@@ -60,8 +83,8 @@ function App() {
       <br />
       <br />
       <hr />
-      <WhoAmI name={{firstName: 'John'}} surname="Smith" link="facebook.com"/>
-      <WhoAmI name={{firstName: 'Alex'}} surname="Shepard" link="facebook.com/1"/>
+      <WhoAmI name='John' surname="Smith" link="facebook.com"/>
+      <WhoAmI name='Alex' surname="Shepard" link="facebook.com/1"/>
     </div>
   );
 }
