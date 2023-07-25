ed7ddc3cb45a5bf4b7c06d2b00c68c21677f4e05
Dmytro <dmitry.khandogy@gmail.com>
2023-07-25 20:28:38 +0200

    Lesson 132 - Events in React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -47,6 +47,7 @@ class WhoAmI extends Component {
     this.state = {
       years: 27,
       text: '+++',
+      position: '',
     }
   }
 
@@ -56,16 +57,33 @@ class WhoAmI extends Component {
     }))
   }
 
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value,
+    })
+  }
+
   render() {
     const {name, surname, link} = this.props;
+    const {position, years} = this.state;
+
+    console.log(this);
     return (
       <div>
         <br />
         <br />
         
         <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name}, surname is - {surname}, age - {this.state.years}</h1>
+        <h1>My name is {name}, surname is - {surname}, 
+            age - {years}, 
+            position - {position}</h1>
         <a href={link}>My profile</a>
+
+        <form>
+          <span>Input position</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'yellow')} />
+        </form>
 
       </div>
     )
