6788333ff984970b034280a13d5b95ae1987f869
Dmytro <dmitry.khandogy@gmail.com>
2023-09-11 13:57:33 +0200

    useReducer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,73 @@
-import {useState} from 'react';
+import {useState, useReducer} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
-import Form from './Form';
-import dataContext from "./context";    
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'toggle':
+            return {autoplay: !state.autoplay}
+    
+        case 'slow':
+            return {autoplay: 300}
 
+        case 'fast':
+            return {autoplay: 700}
 
-const {Provider} = dataContext;
+        case 'custom':
+            return {autoplay: action.payload}
+            
+        default:
+            throw new Error();
+    }
+}
 
-function App() {
-    const [data, setData] = useState({
-        mail: "name@example.com",
-        text: 'some text',
-        forceChangeMail: forceChangeMail,
-    });
+function init(initial) {
+    return {autoplay: initial};
+}
 
-    function forceChangeMail() {
-        setData({...data, mail: "test@example.com"})
+const Slider = ({initial}) => {
+    const [slide, setSlide] = useState(0);
+    // const [autoplay, setAutoplay] = useState(false);
+    const [autoplay, dispatch] = useReducer(reducer, initial, init);
+
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
     }
 
     return (
-        <Provider value={data} >
-            <Form text={data.text} />
-            <button 
-                onClick={() => setData({
-                    mail: "second@example.com",
-                    text: 'another text',
-                    forceChangeMail: forceChangeMail,
-                })}>
-                Click me
-            </button>
-        </Provider>
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'fast'})}>fast autoplay</button>
+                    
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={(e) => dispatch({type: 'custom', payload: +e.target.textContent})}>1000</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+function App() {
+    return (
+        <Slider initial={false} />
     );
 }
 
